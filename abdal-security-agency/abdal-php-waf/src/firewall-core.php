4d17db3b06e750c2700695afb373484ca7ceff49
Abdal.Group <Abdal.Group@Gmail.Com>
2020-08-30 06:49:48 +0430

    Add  1.0

---
+++ abdal-security-agency/abdal-php-waf/src/firewall-core.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by Abdal Security Group.
+ * Programmer: Ebrahim Shafiei
+ * Programmer WebSite: https://hackers.zone/
+ * Programmer Email: Prof.Shafiei@Gmail.com
+ * License : AGCL
+ */
+
+
+namespace ABDALPHPWAF;
+
+class Security{
+    public static function test(){
+        echo "Hey";
+    }
+
+
+}
+//require_once __DIR__. '/Classes/AntiXSS.php';
+
+//$antiXss = ANTI_XSS_CORE\AntiXSS::xss_clean($_GET['url']);
