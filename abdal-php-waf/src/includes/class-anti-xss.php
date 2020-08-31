451830e6862973f98e5686d26c036143f2cc10e3
Abdal.Group <Abdal.Group@Gmail.Com>
2020-08-31 21:30:16 +0430

    v1.3

--- abdal-php-waf/src/includes/class-anti-xss.php
+++ abdal-php-waf/src/includes/class-anti-xss.php
@@ -19,21 +19,6 @@ class Anti_XSS extends Central_Utility
 
     public static function xss_detector($uri)
     {
-//        $regex = array(
-//            '&amp;',
-//            '&lt;',
-//            '&gt;',
-//            '/(&#*\w+)[\x00-\x20]+;/u',
-//            '/(&#x*[0-9A-F]+);*/iu',
-//            '#(<[^>]+?[\x00-\x20"\'])(?:on|xmlns)[^>]*+>#iu',
-//            '#([a-z]*)[\x00-\x20]*=[\x00-\x20]*([`\'"]*)[\x00-\x20]*j[\x00-\x20]*a[\x00-\x20]*v[\x00-\x20]*a[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu',
-//            '#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*v[\x00-\x20]*b[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu',
-//            '#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*-moz-binding[\x00-\x20]*:#u',
-//            '#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?expression[\x00-\x20]*\([^>]*+>#i',
-//            '#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?behaviour[\x00-\x20]*\([^>]*+>#i',
-//            '#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:*[^>]*+>#iu',
-//            '#</*\w+:\w[^>]*+>#i',
-//            '#</*(?:applet|b(?:ase|gsound|link)|embed|frame(?:set)?|i(?:frame|layer)|l(?:ayer|ink)|meta|object|s(?:cript|tyle)|title|xml)[^>]*+>#i');
 
         $xss_pattern = array(
             "%27", // '
